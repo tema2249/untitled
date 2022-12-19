4efd17607b9a0921a4b990bfe478f9b9c701ba96
tem2249 <tem2249@gmail.com>
2022-12-19 09:50:23 +0300

    git config --global user.email "tem2249@gmail.com" git config --global user.name "tem2249"

---
+++ src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    public int height = 180;
+    public void say (String name){
+        System.out.println("Hello, " + name);
+    }
+    public Person (){}
+    public Person (int h){
+        height = h;
+    }
+}
